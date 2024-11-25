f011e052821c6297094346d06f32cc308d0ead7a
Matin-PC <matink1490@gmail.com>
2024-11-25 17:09:02 +0330

    Handle multiple and radio box selection.

--- src/view/components/Registration/Steps/DynamicStep.vue
+++ src/view/components/Registration/Steps/DynamicStep.vue
@@ -1,9 +1,11 @@
 <script>
 import BaseTextField from "@/view/widget/Base/BaseTextField.vue";
+import BaseTextArea from "../../../widget/Base/BaseTextArea.vue";
+import BaseSelect from "../../../widget/Base/BaseSelect.vue";
 
 export default {
   name: "DynamicStep",
-  components: {BaseTextField},
+  components: {BaseSelect, BaseTextArea, BaseTextField},
   props: {
     formItems: Array,
   },
@@ -35,12 +37,58 @@ export default {
       <div class="v-row">
         <template v-for="item in formItems">
           <base-text-field
+              v-if="item.type === 1"
               :label="item.label"
               :class="item.size"
               v-model="model[item.key]"
               :required-symbol="item.isRequired"
               :rules="rulesGenerator(item)"
           />
+          <base-text-area
+              v-if="item.type === 2"
+              :label="item.label"
+              :class="item.size"
+              v-model="model[item.key]"
+              :required-symbol="item.isRequired"
+              :rules="rulesGenerator(item)"
+          />
+
+
+          <template v-if="item.type === 5">
+            <div>
+              <label>{{ item.label }}</label>
+            </div>
+            <div class="d-block">
+              <v-radio-group
+                  :class="item.size"
+                  direction="horizontal"
+                  v-model="model[item.key]"
+                  :rules="rulesGenerator(item)">
+                <v-radio
+                    v-for="childItem in item.children"
+                    :label="childItem.text"
+                    :value="childItem.value"
+                    :key="childItem.value"
+                />
+              </v-radio-group>
+            </div>
+          </template>
+
+
+          <template v-if="[6, 7].includes(item.type)">
+            <base-select
+                :label="item.label"
+                :class="item.size"
+                :items="item.children"
+                v-model="model[item.key]"
+                item-title="text"
+                item-value="value"
+                :multiple="item.type === 7"
+                :required-symbol="item.isRequired"
+                :rules="rulesGenerator(item)"
+            />
+          </template>
+
         </template>
       </div>
     </v-container>
